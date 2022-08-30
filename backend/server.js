06a93fccc2832ad6dbce0eb380a3205b882cfaed
Sahil Pingale <sahilpingale@gmail.com>
2022-08-30 16:53:12 +0530

    frontend and backend apps setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// Use "type: "module" in package.json to use 'import'
+import express from 'express'
+import color from 'colors'
+import dotenv from 'dotenv'
+
+// Configure Environment variables
+dotenv.config()
+
+const app = express()
+
+// In order to use 'req.body'
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Api is running')
+})
+
+const PORT = 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
